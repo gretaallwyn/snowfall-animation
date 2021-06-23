77dfc0d2269145a9a4bc9ee648f61c2965c6efa5
gretaallwyn <68838318+gretaallwyn@users.noreply.github.com>
2021-06-23 13:19:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,50 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+var canvas;
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var engine,world;
+
+var maxDrops = 20;
+
+var snow = [];
+
+var bg1,bg2,bg3;
+
+
+function preload(){
+   
+    //snow1 = loadImage("snow4.webp");
+    //snow2 = loadImage("snow5.webp");
+
+    bg1 = loadImage("snow1.jpg");
+    bg2 = loadImage("snow2.jpg");
+    bg3 = loadImage("snow3.jpg");
+    
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function setup(){
+   canvas = createCanvas(windowWidth,windowHeight);
+   engine = Engine.create();
+   world = engine.world;
+
+   if(frameCount%120===0){
+    for(var i = 0;i<maxDrops;i++){
+        snow.push(new Snow(random(0,1600),random(0,200)));  
+    }
 }
+
+}
+
+function draw(){
+    background(bg2);
+    Engine.update(engine);
+
+        for(var i = 0;i<maxDrops;i++){
+            snow[i].display();
+            snow[i].update();    
+        }
+    
+
+    drawSprites();
+}   
